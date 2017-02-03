a501b249180402f58d62ef1777f2adaf2a9cc995
Patrick Kelly <patrick.charles.kelly@gmail.com>
2017-02-03 17:24:31 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    require_once dirname(__FILE__).'/vendor/twig/twig/lib/Twig/Autoloader.php';
+    Twig_Autoloader::register();
+    
+    $loader = new Twig_Loader_Filesystem(dirname(__FILE__).'/templates');
+    $twig = new Twig_Environment($loader, array(
+        'cache' => false,
+    ));
+?>
+<html>
+    <head>
+        <title>PiFeud</title>
+    </head>
+    <body>
+        <h1>Welcome to the Feud!</h1>
+        <?php
+         $template = $twig->load('testTemplate.html');
+         $templateVars = array(
+            'templateVar' => 'Hello World!!'
+         );
+         echo $template->render($templateVars);
+        ?>
+    </body>
+</html>
